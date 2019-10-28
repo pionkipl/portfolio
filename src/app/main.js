4c0aaf526794639cf58366809a78a69a96283642
pionkipl <jarek491@gmail.com>
2019-10-28 13:25:59 +0100

    Add animated background.

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,5 @@
 const sendBtn = document.querySelector('.contact-button');
+const portfolio = document.querySelectorAll('.examples__card');
 
 sendBtn.addEventListener('click', () => {
   window.open('mailto:jarek491@gmail.com');
@@ -6,3 +7,12 @@ sendBtn.addEventListener('click', () => {
 
 console.log('%cHello there. I wanted to place my phone here so no random users would get it: Here is mine: %c 791 571 174',
 'font-size: 18px', 'font-size: 26px; color: white; background: red');
+
+if (window.innerHeight < 650 && window.innerWidth > 800) {
+  portfolio.forEach(el => {
+    el.style.width = "22%"
+  })
+}
+
+
+// animate backgorund
